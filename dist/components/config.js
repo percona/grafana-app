ae3fd017f1fc65693dea3aaa6c025df097aa38e6
Roman Vynar <roman.vynar@percona.com>
2016-05-25 14:30:11 +0300

    Initial commit

---
+++ dist/components/config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var ExampleAppConfigCtrl = exports.ExampleAppConfigCtrl = function ExampleAppConfigCtrl() {
+  _classCallCheck(this, ExampleAppConfigCtrl);
+};
+
+ExampleAppConfigCtrl.templateUrl = 'components/config.html';
+//# sourceMappingURL=config.js.map
